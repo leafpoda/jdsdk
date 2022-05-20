1b28dd46f2c26f4de1e8a132887ab008ae2777a5
王东芳 <wangdongfang@192.168.1.5>
2022-05-20 18:04:57 +0800

    jd 获取dmp 报表

---
+++ src/jd/request/AdsIbgUniversalJosServiceDmpDetailV1Request.php
@@ -0,0 +1,199 @@
+<?php
+class AdsIbgUniversalJosServiceDmpDetailV1Request
+{
+
+
+	private $apiParas = array();
+	
+	public function getApiMethodName(){
+	  return "jingdong.ads.ibg.UniversalJosService.dmpDetail.v1";
+	}
+	
+	public function getApiParas(){
+	    if(empty($this->apiParas)){
+            return "{}";
+        }
+        return json_encode($this->apiParas);
+	}
+	
+	public function check(){
+		
+	}
+	
+	public function putOtherTextParam($key, $value){
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+
+    private $version;
+
+    public function setVersion($version){
+        $this->version = $version;
+    }
+
+    public function getVersion(){
+        return $this->version;
+    }
+                                    	                   			private $clickOrOrderCaliber;
+    	                        
+	public function setClickOrOrderCaliber($clickOrOrderCaliber){
+		$this->clickOrOrderCaliber = $clickOrOrderCaliber;
+         $this->apiParas["clickOrOrderCaliber"] = $clickOrOrderCaliber;
+	}
+
+	public function getClickOrOrderCaliber(){
+	  return $this->clickOrOrderCaliber;
+	}
+
+                        	                   			private $clickOrOrderDay;
+    	                        
+	public function setClickOrOrderDay($clickOrOrderDay){
+		$this->clickOrOrderDay = $clickOrOrderDay;
+         $this->apiParas["clickOrOrderDay"] = $clickOrOrderDay;
+	}
+
+	public function getClickOrOrderDay(){
+	  return $this->clickOrOrderDay;
+	}
+
+                        	                   			private $startDay;
+    	                        
+	public function setStartDay($startDay){
+		$this->startDay = $startDay;
+         $this->apiParas["startDay"] = $startDay;
+	}
+
+	public function getStartDay(){
+	  return $this->startDay;
+	}
+
+                        	                   			private $endDay;
+    	                        
+	public function setEndDay($endDay){
+		$this->endDay = $endDay;
+         $this->apiParas["endDay"] = $endDay;
+	}
+
+	public function getEndDay(){
+	  return $this->endDay;
+	}
+
+                        	                   			private $isDaily;
+    	                        
+	public function setIsDaily($isDaily){
+		$this->isDaily = $isDaily;
+         $this->apiParas["isDaily"] = $isDaily;
+	}
+
+	public function getIsDaily(){
+	  return $this->isDaily;
+	}
+
+                        	                   			private $page;
+    	                        
+	public function setPage($page){
+		$this->page = $page;
+         $this->apiParas["page"] = $page;
+	}
+
+	public function getPage(){
+	  return $this->page;
+	}
+
+                        	                   			private $pageSize;
+    	                        
+	public function setPageSize($pageSize){
+		$this->pageSize = $pageSize;
+         $this->apiParas["pageSize"] = $pageSize;
+	}
+
+	public function getPageSize(){
+	  return $this->pageSize;
+	}
+
+                        	                   			private $displayType;
+    	                        
+	public function setDisplayType($displayType){
+		$this->displayType = $displayType;
+         $this->apiParas["displayType"] = $displayType;
+	}
+
+	public function getDisplayType(){
+	  return $this->displayType;
+	}
+
+                        	                   			private $orderStatusCategory;
+    	                        
+	public function setOrderStatusCategory($orderStatusCategory){
+		$this->orderStatusCategory = $orderStatusCategory;
+         $this->apiParas["orderStatusCategory"] = $orderStatusCategory;
+	}
+
+	public function getOrderStatusCategory(){
+	  return $this->orderStatusCategory;
+	}
+
+                        	                   			private $businessType;
+    	                        
+	public function setBusinessType($businessType){
+		$this->businessType = $businessType;
+         $this->apiParas["businessType"] = $businessType;
+	}
+
+	public function getBusinessType(){
+	  return $this->businessType;
+	}
+
+                        	                   			private $product;
+    	                        
+	public function setProduct($product){
+		$this->product = $product;
+         $this->apiParas["product"] = $product;
+	}
+
+	public function getProduct(){
+	  return $this->product;
+	}
+
+                        	                   			private $dmpId;
+    	                        
+	public function setDmpId($dmpId){
+		$this->dmpId = $dmpId;
+         $this->apiParas["dmpId"] = $dmpId;
+	}
+
+	public function getDmpId(){
+	  return $this->dmpId;
+	}
+
+                        	                        	                        	                   			private $authType;
+    	                        
+	public function setAuthType($authType){
+		$this->authType = $authType;
+         $this->apiParas["authType"] = $authType;
+	}
+
+	public function getAuthType(){
+	  return $this->authType;
+	}
+
+                        	                   			private $accessPin;
+    	                        
+	public function setAccessPin($accessPin){
+		$this->accessPin = $accessPin;
+         $this->apiParas["accessPin"] = $accessPin;
+	}
+
+	public function getAccessPin(){
+	  return $this->accessPin;
+	}
+
+                        	                        	                        	                        	}
+
+
+
+
+
+        
+ 
+
