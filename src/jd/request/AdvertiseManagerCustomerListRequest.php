a23a8489f26806e1cfdce8da7f988f41047b56a2
baihaiyin <baihaiyin@linkworld.cn>
2025-07-10 16:59:06 +0800

    feat: 增加新的sdk文件

---
+++ src/jd/request/AdvertiseManagerCustomerListRequest.php
@@ -0,0 +1,89 @@
+<?php
+class AdvertiseManagerCustomerListRequest
+{
+
+
+	private $apiParas = array();
+	
+	public function getApiMethodName(){
+	  return "jingdong.advertise.manager.customer.list";
+	}
+	
+	public function getApiParas(){
+	    if(empty($this->apiParas)){
+            return "{}";
+        }
+        return $this->apiParas;
+	}
+	
+	public function check(){
+		
+	}
+	
+	public function putOtherTextParam($key, $value){
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+
+    private $version;
+
+    public function setVersion($version){
+        $this->version = $version;
+    }
+
+    public function getVersion(){
+        return $this->version;
+    }
+                                    	                        	                        	                        	                        	                        	                   			private $authType;
+    	                        
+	public function setAuthType($authType){
+		$this->authType = $authType;
+         $this->apiParas["authType"] = $authType;
+	}
+
+	public function getAuthType(){
+	  return $this->authType;
+	}
+
+                        	                   			private $accessPin;
+    	                        
+	public function setAccessPin($accessPin){
+		$this->accessPin = $accessPin;
+         $this->apiParas["accessPin"] = $accessPin;
+	}
+
+	public function getAccessPin(){
+	  return $this->accessPin;
+	}
+
+                        	                        	                   			private $pageIndex;
+    	                        
+	public function setPageIndex($pageIndex){
+		$this->pageIndex = $pageIndex;
+         $this->apiParas["pageIndex"] = $pageIndex;
+	}
+
+	public function getPageIndex(){
+	  return $this->pageIndex;
+	}
+
+                        	                   			private $pageSize;
+    	                        
+	public function setPageSize($pageSize){
+		$this->pageSize = $pageSize;
+         $this->apiParas["pageSize"] = $pageSize;
+	}
+
+	public function getPageSize(){
+	  return $this->pageSize;
+	}
+
+                        	                        	}
+
+
+
+
+
+        
+ 
+
